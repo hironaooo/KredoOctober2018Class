a1f05e6321ddf3591afcd5d1a1dde0fb8745bdc5
上原大尚 <ueharadainao@MacBook-Pro-Ue.local>
2018-10-05 12:25:19 +0800

    First php commit

---
+++ start.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<?php  
+	echo "<h1>Hello, Welcome to Kredo</h1>";
+	echo "<p>Hello, Welcome to Kredo</p>";
+?>
+<hr>
+<?php  
+	echo "<div class='banner'>";
+		echo "<div class=\"banner\">example</div>";
+		echo '<div class="banner">single quote</div>';
+	echo "</div>";
+
+?>
+</body>
+</html>
